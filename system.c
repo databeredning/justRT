b0074590d88eef01f9423141134c222c67a9b883
Per Mellander <per.mellander@cpacsystems.se>
2026-08-13 15:12:11 +0200

    Preserve EXC_RETURN in PendSV across scheduler helper call

--- system.c
+++ system.c
@@ -127,10 +127,12 @@ void PendSV_Handler(void)
 {
     __asm volatile (
         "mrs r0, psp\n"
+        "push {lr}\n"
         "stmdb r0!, {r4-r11}\n"
         "bl kernel_pendsv_switch\n"
         "ldmia r0!, {r4-r11}\n"
         "msr psp, r0\n"
-        "bx lr\n"
+        "pop {r3}\n"
+        "bx r3\n"
     );
 }
